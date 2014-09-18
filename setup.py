81e76c6c19b32ef495f98cbeaca2db2d5d139c24
Matthew Scragg <scragg@gmail.com>
2014-09-18 17:54:34 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 CLASSIFIERS = [
     'Intended Audience :: Developers',
