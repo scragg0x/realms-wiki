67bf9eab352f7e0704e95fcd57ec08399f223161
Matthew Scragg <scragg@gmail.com>
2014-09-26 16:18:48 -0500

    updated docker runsv script updated dockerfile

--- docker/realms-wiki.sh
+++ docker/realms-wiki.sh
@@ -6,4 +6,4 @@ if [ "${REALMS_WIKI_CONFIG}" != "" ]; then
     realms-wiki configure ${REALMS_WIKI_CONFIG}
 fi
 
-exec /sbin/setuser deploy /home/deploy/realms-wiki/.venv/bin/python realms-wiki run >>/var/log/realms-wiki/realms-wiki.log 2>&1
+exec /sbin/setuser deploy /home/deploy/realms-wiki/.venv/bin/python manage.py run >>/var/log/realms-wiki/realms-wiki.log 2>&1
