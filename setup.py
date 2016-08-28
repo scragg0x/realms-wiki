1fef1b754ce24a76a30fb98be669ade75c2a9d60
Stephane Martin <stephane.martin@vesperal.eu>
2016-08-28 16:49:26 +0200

    added six as an explicit dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ setup(name='realms-wiki',
           'itsdangerous==0.24',
           'markdown2==2.3.1',
           'python-ldap==2.4.22',
-          'simplejson==3.6.3'
+          'simplejson==3.6.3',
+          'six=1.10.0'
       ],
       entry_points={
           'console_scripts': [
